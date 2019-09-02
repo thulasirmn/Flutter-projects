381e9b4da8bc19d42af3b8aa1ad61ea406cc1712
Thulasi, Raman <thulasirmn8@gmail.com>
2019-09-02 13:09:38 +0530

    inital commit

---
+++ lib/demo.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/animation.dart';
+import 'package:flutter/material.dart';
+
+class demo extends StatefulWidget {
+  @override
+  _demoState createState() => _demoState();
+}
+
+class _demoState extends State<demo>
+    with SingleTickerProviderStateMixin {
+  AnimationController _controller;
+
+  @override
+  void setState(fn) {
+    // TODO: implement setState
+    var todo = "todo value";
+    
+  }
+ 
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      
+    );
+  }
+}
